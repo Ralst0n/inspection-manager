663af160b4c7565da713e220af155dfe6080665c
Ralst0n <pghRalston@gmail.com>
2018-03-26 16:30:36 -0400

    Add create & update paths

--- apps/equipment/urls.py
+++ apps/equipment/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.EquipmentListView.as_view(), name="equipment"),
     url(r'^(?P<pk>\d+)', views.EquipmentDetailView.as_view(), name="equipment-detail"),
+    url(r'^create$', views.EquipmentCreateView.as_view(), name='equipment-create'),
+    url(r'^update/(?P<pk>\d+)$', views.EquipmentUpdateView.as_view(), name='equipment-update'),
 ]
