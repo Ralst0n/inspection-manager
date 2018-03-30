efc2e8ee8693fb68cd47b90653cd305379bb27bc
Ralst0n <pghRalston@gmail.com>
2018-03-30 09:38:15 -0400

    update path to media storage

--- config/settings/production.py
+++ config/settings/production.py
@@ -25,7 +25,8 @@ ALLOWED_HOSTS = ['prudentoffice.herokuapp.com',]
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_S3_CUSTOM_DOMAIN = f"{config('AWS_STORAGE_BUCKET_NAME')}.s3.amazonaws.com"
+AWS_BUCKET_NAME = 'prudent-office-assets'
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_BUCKET_NAME}.s3.amazonaws.com"
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
