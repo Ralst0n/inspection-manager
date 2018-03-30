40e7a54dc560e0d41f0a07d31cb764d76fff836b
Ralst0n <pghRalston@gmail.com>
2018-03-30 11:19:24 -0400

    update path to media storage

--- config/settings/production.py
+++ config/settings/production.py
@@ -35,7 +35,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 
 AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 #STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 # MEDIA_ROOT = f"https://{'AWS_S3_CUSTOM_DOMAIN'}/media"
