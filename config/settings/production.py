8475b285307c69dc9e29f9164ef7f18f39396806
Ralst0n <pghRalston@gmail.com>
2018-03-30 10:58:35 -0400

    update path to media storage

--- config/settings/production.py
+++ config/settings/production.py
@@ -23,13 +23,20 @@ EMAIL_USE_TLS = True
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 ALLOWED_HOSTS = ['prudentoffice.herokuapp.com',]
 
+
+
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_BUCKET_NAME = 'prudent-office-assets'
-AWS_S3_CUSTOM_DOMAIN = f"http://{'AWS_STORAGE_BUCKET_NAME'}.s3.amazonaws.com"
+AWS_STORAGE_BUCKET_NAME = 'prudent-office-assets'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
-MEDIA_ROOT = f"https://{'AWS_S3_CUSTOM_DOMAIN'}/media"
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+# MEDIA_ROOT = f"https://{'AWS_S3_CUSTOM_DOMAIN'}/media"
 DEFAULT_FILE_STORAGE = 'config.settings.storage_backends.MediaStorage' 
