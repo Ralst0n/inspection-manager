8308f1efa7cdfe8ff801dee871500dc963b3418a
Ralst0n <pghRalston@gmail.com>
2018-05-23 12:08:13 -0400

    create PlannedProjectAdmin

--- apps/partners/admin.py
+++ apps/partners/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import BusinessPartner, LetProject, PlannedProject, ProjectTeam
 # Register your models here.
 admin.site.register(LetProject)
-admin.site.register(PlannedProject)
+
+class PlannedProjectAdmin(admin.ModelAdmin):
+    list_display = ('agreement_number', 'scrapped_date', 'district')
+admin.site.register(PlannedProject, PlannedProjectAdmin)
+
 admin.site.register(BusinessPartner)
 admin.site.register(ProjectTeam)
