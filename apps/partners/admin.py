bdb34a94ec763ad3f706760254b6b5f1ebc7f29d
Ralst0n <pghRalston@gmail.com>
2018-05-25 11:07:49 -0400

    Add sorting by district and winner to LetProject

--- apps/partners/admin.py
+++ apps/partners/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import BusinessPartner, LetProject, PlannedProject, ProjectTeam
 # Register your models here.
-admin.site.register(LetProject)
+
+class LetProjectAdmin(admin.ModelAdmin):
+    list_display = ("agreement_number", "district", "winner")
+    list_filter = ("district", "winner")
+admin.site.register(LetProject, LetProjectAdmin)
 
 class PlannedProjectAdmin(admin.ModelAdmin):
     list_display = ('agreement_number', 'scrapped_date', 'district')
