d2239b0f9257d3aca556c792850f060888c0e9ff
Ralst0n <pghRalston@gmail.com>
2018-07-02 16:49:42 -0400

    add rest_framework routers

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -1,8 +1,15 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
+from django.urls import path
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'api', views.ProjectViewSet)
 
 urlpatterns = [
     url(r'^$', views.ProjectsListView.as_view(), name='projects'),
     url(r'^search/$', views.ProjectSearchView.as_view(), name='search-projects'),
     url(r'^(?P<pk>\d{3}.\d{3})', views.ProjectsDetailView.as_view(), name='project-detail'),
 ]
+
+urlpatterns += router.urls
