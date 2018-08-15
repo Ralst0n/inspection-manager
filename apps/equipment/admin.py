74d42188517a60bf28004ff49c50ce21c5b4e46e
Ralst0n <pghRalston@gmail.com>
2018-08-15 09:40:24 -0400

    add checkouts to equipment page

--- apps/equipment/admin.py
+++ apps/equipment/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Equipment, Checkout
 # Register your models here.
+class CheckoutInline(admin.TabularInline):
+    model = Checkout
+
 @admin.register(Equipment)
 class EquipmentAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
@@ -15,7 +18,7 @@ class EquipmentAdmin(admin.ModelAdmin):
         else:
             return Equipment.objects.all().filter(office__in=groups)
     list_display = ('name', 'device', 'serial_number')
-
+    inlines = [CheckoutInline]
 @admin.register(Checkout)
 class CheckoutAdmin(admin.ModelAdmin):
     pass
