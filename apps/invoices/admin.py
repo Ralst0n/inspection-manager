cf0b5f28ca8ff3bcf59326ca11e64fc5b5f54e7d
Ralst0n <pghRalston@gmail.com>
2018-08-15 10:09:18 -0400

    remove Comment register, add inline to InvoiceAdmin

--- apps/invoices/admin.py
+++ apps/invoices/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from .models import Invoice, Comments
 # Register your models here.
 # admin.site.register(Invoice)
+class CommentInline(admin.TabularInline):
+    model = Comments
+    extra = 1
 
 class InvoiceAdmin(admin.ModelAdmin):
     list_display = ('project', 'estimate_number', 'status')
     list_filter = ('project', 'status')
+    inlines = [CommentInline]
+
 admin.site.register(Invoice, InvoiceAdmin)
-admin.site.register(Comments)
