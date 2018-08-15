ddddf8e272684b0302c7c08e92bdd67a820635ee
Ralst0n <pghRalston@gmail.com>
2018-08-15 10:08:21 -0400

    remove Notes & History register, add inlines to Inspector

--- apps/inspectors/admin.py
+++ apps/inspectors/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from .models import Inspector, Notes, History
 # Register your models here.
-admin.site.register(Notes)
 
-admin.site.register(History)
+class NoteInline(admin.TabularInline):
+    model = Notes
+    extra = 1
+
+class HistoryInline(admin.TabularInline):
+    model = History
+    extra = 0
 
 @admin.register(Inspector)
 class InspectorAdmin(admin.ModelAdmin):
     model = Inspector
     list_display = ('first_name', 'last_name', 'classification')
+    inlines = [HistoryInline, NoteInline]
