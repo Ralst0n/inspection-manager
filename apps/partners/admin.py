eb10db35e1db58898414896b100605f1d95c4b94
Ralst0n <pghRalston@gmail.com>
2018-08-24 11:16:47 -0400

    add let_date to LetProjectAdmin

--- apps/partners/admin.py
+++ apps/partners/admin.py
@@ -3,8 +3,8 @@ from .models import BusinessPartner, LetProject, PlannedProject, ProjectTeam
 # Register your models here.
 
 class LetProjectAdmin(admin.ModelAdmin):
-    list_display = ("agreement_number", "district", "winner")
-    list_filter = ("district", "winner")
+    list_display = ("agreement_number", "district", "winner", "let_date")
+    list_filter = ("district", "let_date")
 admin.site.register(LetProject, LetProjectAdmin)
 
 class PlannedProjectAdmin(admin.ModelAdmin):
